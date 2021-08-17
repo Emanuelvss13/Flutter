8ae08038a4422898495059e3f1bf43021ac2d57c
Emanuelvss13 <emanuelsouza2002@gmail.com>
2021-08-17 13:21:17 -0300

    finish project

---
+++ counter_app/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MaterialApp(
+//       theme: ThemeData(primarySwatch: Colors.pink),
+//       home: HomePage()
+//   )
+// }
+
+void main() {
+  runApp(MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.pink), home: HomePage()));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var count = 0;
+
+  void increment() {
+    count++;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Meu Primerio App'),
+        ),
+        body: Container(
+          child: Center(
+            child: Text(
+              "Contador\n$count",
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () {
+            increment();
+          },
+        ));
+  }
+}
